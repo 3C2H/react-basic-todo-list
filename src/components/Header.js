a9087b40e47bf6e91fc1a3db9322d3cebe83d49b
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-02-28 16:42:24 +0100

    component 'Header' creation

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1 className='title'> A simple REACT Todo List </h1>
+      </div>
+    )
+  }
+}
+
+export default Header

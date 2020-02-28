63ad9c5ab5705e295f9b8ca3ad3e802a506727da
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-02-28 16:50:17 +0100

    component 'TodoList' creation

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class TodoList extends React.Component {
+  render() {
+    return(
+      <div>
+        <div classname='TodoListContainer'>
+          I'm a todo list (or rather will be)
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList

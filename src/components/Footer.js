ca16801e3825262737023d54404a515fd702e037
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-02-28 16:45:33 +0100

    component 'Footer' creation

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <div>
+        <p className='footerMessage'> Nice foots aren't they =8) </p>
+      </div>
+    )
+  }
+}
+
+export default Footer

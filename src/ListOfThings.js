d27c2d83d02df66e83c137a1e9c63ec1b823b2ef
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-03-06 10:23:53 +0100

    created ListOfThings file

---
+++ src/ListOfThings.js
@@ -0,0 +1,26 @@
+module.exports [
+  {
+    id:1,
+    task: "Do the laundry",
+  },
+  {
+    id:2,
+    task: "Grocery shopping",
+  },
+  {
+    id:3,
+    task: "Practice React",
+  },
+  {
+    id:4,
+    task: "Watch a movie",
+  },
+  {
+    id:5,
+    task: "Practice Js",
+  },
+  {
+    id:6,
+    task: "Laundry ironing",
+  }
+]

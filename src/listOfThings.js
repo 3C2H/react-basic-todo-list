d27e85216825818737fa2e8121eb9787cc48becf
3c2h <Charles_Chiacchia_student2018@wilder.school>
2020-03-06 11:01:23 +0100

    update listOfThings with a 'completed' status

--- src/listOfThings.js
+++ src/listOfThings.js
@@ -2,25 +2,31 @@ module.exports = [
   {
     id:1,
     task: "Do the laundry",
+    completed : true
   },
   {
     id:2,
     task: "Grocery shopping",
+    completed : false
   },
   {
     id:3,
     task: "Practice React",
+    completed : false
   },
   {
     id:4,
     task: "Watch a movie",
+    completed : false
   },
   {
     id:5,
     task: "Practice Js",
+    completed : false
   },
   {
     id:6,
     task: "Laundry ironing",
+    completed : false
   }
 ]
